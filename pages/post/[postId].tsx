b44a1aa7838a3637aff986dcc2bcf80f48b040ac
Yuriy Yurich <63981542+shzFas@users.noreply.github.com>
2022-07-22 13:30:52 +0600

    1

--- pages/post/[postId].tsx
+++ pages/post/[postId].tsx
@@ -89,7 +89,7 @@ function PostPage() {
         <div className="-my-5 rounded-b-md border border-t-0 border-gray-300 bg-white py-5 px-10">
             <hr className='py-2' />
 
-            {post?.comments.map((comment) => (
+            {/* {post?.comments.map((comment) => (
                 <div
                     className='relative flex items-center space-x-2 space-y-5'
                     key={comment.id}
@@ -107,7 +107,7 @@ function PostPage() {
                         <p>{comment.text}</p>
                     </div>
                 </div>
-            ))}
+            ))} */}
         </div>
     </div>
   )
